3877433aae4c6e96ade9e59c9d68c641e25e30d6
Dmytro Chornovol <dbtheoldb@gmail.com>
2023-11-29 19:17:33 +0200

    added ui for course tools/hangouts

--- src/product-tours/newUserCourseHomeTour/LaunchCourseHomeTourButton.jsx
+++ src/product-tours/newUserCourseHomeTour/LaunchCourseHomeTourButton.jsx
@@ -8,22 +8,17 @@ import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import { Button, Icon } from '@edx/paragon';
 import { Compass } from '@edx/paragon/icons';
 
+import { Typography } from '@mui/material';
 import { useModel } from '../../generic/model-store';
 import { launchCourseHomeTour } from '../data/slice';
 import messages from '../messages';
 
 const LaunchCourseHomeTourButton = ({ intl, srOnly }) => {
-  const {
-    courseId,
-  } = useSelector(state => state.courseHome);
+  const { courseId } = useSelector(state => state.courseHome);
 
-  const {
-    org,
-  } = useModel('courseHomeMeta', courseId);
+  const { org } = useModel('courseHomeMeta', courseId);
 
-  const {
-    toursEnabled,
-  } = useSelector(state => state.tours);
+  const { toursEnabled } = useSelector(state => state.tours);
 
   const dispatch = useDispatch();
 
@@ -43,15 +38,16 @@ const LaunchCourseHomeTourButton = ({ intl, srOnly }) => {
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
       {toursEnabled && (
-        <Button variant="link" size="inline" className={`p-0 ${srOnly && 'sr-only sr-only-focusable'}`} onClick={handleClick}>
+        <Button
+          variant="link"
+          size="inline"
+          className={`p-0 ${srOnly && 'sr-only sr-only-focusable'}`}
+          onClick={handleClick}
+        >
           {!srOnly && (
-            <Icon
-              src={Compass}
-              className="mr-2"
-              style={{ height: '18px', width: '18px' }}
-            />
+            <Icon src={Compass} className="mr-2" style={{ height: '18px', width: '18px' }} />
           )}
-          {intl.formatMessage(messages.launchTour)}
+          <Typography fontSize="15px">{intl.formatMessage(messages.launchTour)}</Typography>
         </Button>
       )}
     </>
