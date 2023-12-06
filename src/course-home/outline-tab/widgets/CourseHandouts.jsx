c2f9b42c081be3eb99dbef0db0489f923a1d2118
Dmytro Chornovol <dbtheoldb@gmail.com>
2023-12-06 15:17:40 +0200

    added ui for course tools/hangouts

--- src/course-home/outline-tab/widgets/CourseHandouts.jsx
+++ src/course-home/outline-tab/widgets/CourseHandouts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import { Typography } from '@mui/material';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 
 import LmsHtmlFragment from '../LmsHtmlFragment';
@@ -8,12 +8,8 @@ import messages from '../messages';
 import { useModel } from '../../../generic/model-store';
 
 const CourseHandouts = ({ intl }) => {
-  const {
-    courseId,
-  } = useSelector(state => state.courseHome);
-  const {
-    handoutsHtml,
-  } = useModel('outline', courseId);
+  const { courseId } = useSelector(state => state.courseHome);
+  const { handoutsHtml } = useModel('outline', courseId);
 
   if (!handoutsHtml) {
     return null;
@@ -21,9 +17,11 @@ const CourseHandouts = ({ intl }) => {
 
   return (
     <section className="mb-4">
-      <h2 className="h4">{intl.formatMessage(messages.handouts)}</h2>
+      <Typography fontSize="18px" fontWeight={700} color="#1A2029" fontFamily="Hind">
+        {intl.formatMessage(messages.handouts)}
+      </Typography>
       <LmsHtmlFragment
-        className="small"
+        className="medium"
         html={handoutsHtml}
         title={intl.formatMessage(messages.handouts)}
       />
